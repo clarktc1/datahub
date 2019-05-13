06798c348192c2ed1834499a675454e67fd91ec2
sunil <sunil@1wayit.com>
2019-05-13 11:43:05 +0000

    add email view

---
+++ application/views/email/email_template.php
@@ -0,0 +1,74 @@
+<table border="0" width="100%">
+	<tbody>
+		<tr>
+			<td>
+				<table xss=removed border="0" width="600" cellspacing="0" cellpadding="0" align="center">
+					<tbody>
+						<tr>
+							<td xss=removed align="center" valign="middle" height="100">LOGO</td>
+						</tr>
+						<tr>
+							<td>
+								<table width="100%" cellspacing="0" cellpadding="0">
+									<tbody>
+										<tr>
+											<td width="15"> </td>
+											<td width="570">
+												<table width="100%" cellspacing="0" cellpadding="0">
+													<tbody>
+														<tr>
+															<td height="15"> </td>
+														</tr>
+														<tr>
+															<td width="530">
+																<table width="100%" cellspacing="0" cellpadding="0">
+																	<tbody>
+																		<tr>
+																			<td xss=removed>
+																				<table border="0" width="100%" cellspacing="0" cellpadding="0">
+																					<tbody>
+																						<tr>
+																							<td xss=removed align="left" valign="middle" height="30"><strong>Dear FIRSTNAME LASTNAME,</strong></td>
+																						</tr>
+																						<tr>
+																							<td xss=removed align="left" valign="top">
+																								<?php echo $error_msg_html; ?>
+																							</td>
+																						</tr>
+																						<tr>
+																							<td xss=removed align="left" valign="top">Regards,<br> SITE_TITLE Team</td>
+																						</tr>
+																					</tbody>
+																				</table>
+																			</td>
+																		</tr>
+																	</tbody>
+																</table>
+															</td>
+														</tr>
+														<tr>
+															<td xss=removed height="15">
+																<p>*This email account is not monitored. Please do not reply to this email as we will not be able to read and respond to your messages.</p>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</td>
+											<td width="15"> </td>
+										</tr>
+									</tbody>
+								</table>
+							</td>
+						</tr>
+						<tr>
+							<td xss=removed align="center" valign="top">
+								<p>You have received this message by auto generated e-mail.</p>
+								<center>COPY_RIGHT_TEXT</center>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</td>
+		</tr>
+	</tbody>
+</table>
